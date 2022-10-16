cc0a0c5138673a9ffc30355c5690e18db70b4a12
Julien Viatgé <46216088+jviatge@users.noreply.github.com>
2022-10-16 15:39:08 +0200

    Initial commit

---
+++ src/Home.js
@@ -0,0 +1,36 @@
+import React from "react";
+import data from "./data/data.json";
+import { useCMS, useForm, usePlugin } from "tinacms";
+import { InlineForm, InlineBlocks } from "react-tinacms-inline";
+
+import { heroBlock } from "./components/Hero/Hero";
+import { paragraphBlock } from "./components/Paragraph/Paragraph";
+import { imagesBlock } from "./components/Images/Images";
+import { featureListBlock } from "./components/FeatureList/FeatureList";
+export default function Home() {
+  const cms = useCMS();
+
+  const formConfig = {
+    id: "./data/data.json",
+    initialValues: data,
+    onSubmit() {
+      cms.alerts.success("Saved!");
+    }
+  };
+  const [, form] = useForm(formConfig);
+  usePlugin(form);
+
+  return (
+    <div className="home">
+      <InlineForm form={form}>
+        <InlineBlocks name="blocks" blocks={HOME_BLOCKS} />
+      </InlineForm>
+    </div>
+  );
+}
+const HOME_BLOCKS = {
+  hero: heroBlock,
+  images: imagesBlock,
+  paragraph: paragraphBlock,
+  features: featureListBlock
+};
